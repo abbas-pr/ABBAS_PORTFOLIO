0189d3c8d77f900671c9e9dd7df360d9bb12cb78
abbas-pr <abbas0061729@gmail.com>
2026-08-17 07:13:22 -0700

    debug-respons for navbar

--- portfolio/src/component/Navbar.jsx
+++ portfolio/src/component/Navbar.jsx
@@ -6,12 +6,12 @@ import Skill from './section/Skill.jsx'
 export default function Navbar(){
     return (
         <>
-    <nav className="bg-purple-800 text-white p3  flex justify-between items-center h-16 fixed w-full">
-        <div className="relative left-7 font-bold px-2 text-xl">
+    <nav className="bg-purple-800 text-white  flex justify-between items-center h-16 fixed w-full">
+        <div className="relative left-1 md:left-7 font-bold px-2 text-xl">
           <p>PORTFOLIO</p>
         </div>
         <div>
-            <ul className='relative right-4 flex justify-around items-center gap-2 md:gap-5'>
+            <ul className='relative right-1 md:right-4 flex justify-around items-center gap-2 md:gap-5'>
 
            <li className='cursor-pointer'><Link activeClass='active' spy={true} to="About" smooth={true} duration={500} className='font-bold p-1'>About</Link></li>
             <li className='cursor-pointer'><Link activeClass='active' spy={true} to="Project" smooth={true} duration={300} className='  font-bold p-1 rounded-xl  transision-3'>project</Link></li>
