ed198d07a063f091de68ec76438a8b9d8494399d
abbas-pr <abbas0061729@gmail.com>
2026-08-17 07:03:52 -0700

    feature-contact api backend

--- portfolio/src/component/section/contact.jsx
+++ portfolio/src/component/section/contact.jsx
@@ -22,7 +22,7 @@ function Contact() {
     setStatus("Sending...");
 
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
+      const response = await fetch(" https://abbas-portfolio-ij9n.onrender.com/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
